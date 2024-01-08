a5eb1db57b2570780c892b3a50cc65cced30203c
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-01-08 22:06:14 +0300

    Initial Commit

---
+++ GroceriesApp/GroceriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceriesAppApp.swift
+//  GroceriesApp
+//
+//  Created by Dmitry Yastrebov on 08.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct GroceriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
